8f96a8c5328f8fc3a20ff072c26771a67cc98953
humwer <lgmzrk@inbox.ru>
2022-07-24 14:13:59 +0700

    Added vars for ProductPage

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,11 @@ class LoginPageLocators:
     LOGIN_URL = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form')
+
+
+class ProductPageLocators:
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, "#add_to_basket_form > button[type='submit']")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, "p[class='price_color']")
+    PRICE_BASKET = (By.CSS_SELECTOR, "div[class='basket-mini pull-right hidden-xs']")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div[class='col-sm-6 product_main'] h1")
+    PRODUCT_NAME_IN_BASKET = (By.CSS_SELECTOR, "div[class='alertinner ']")
