711fe3b6b29349f3cdfaac815dda596a538db41c
humwer <lgmzrk@inbox.ru>
2022-07-24 15:23:02 +0700

    Updated prints for funcs

--- pages/product_page.py
+++ pages/product_page.py
@@ -12,13 +12,12 @@ class ProductPage(BasePage):
     def product_name_equals_product_name_in_basket(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         product_name_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_BASKET).text
-        assert product_name in product_name_in_basket, f"Incorrect! Price product: {product_name}, " \
+        assert product_name == product_name_in_basket, f"Incorrect! Price product: {product_name}, " \
                                                        f"price basket: {product_name_in_basket}"
-        print('[+] Product name equals product name in basket')
+        print(f'[+] Product name {product_name} equals product name in basket {product_name_in_basket}')
 
     def price_product_equals_price_basket(self):
         price_product = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text
         price_basket = self.browser.find_element(*ProductPageLocators.PRICE_BASKET).text
-        assert price_product in price_basket, f"Incorrect! Price product: {price_product}, price basket: {price_basket}"
-        print('[+] Price of product equals price of basket')
-
+        assert price_product == price_basket, f"Incorrect! Product price: {price_product}, basket price: {price_basket}"
+        print(f'[+] Product price {price_product} equals basket price {price_basket}')
