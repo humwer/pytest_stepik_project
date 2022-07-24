2c8bd9196658cbed482539d480d6c93cdba9c5f0
humwer <lgmzrk@inbox.ru>
2022-07-24 17:40:56 +0700

    Added func for regster new user for LoginPage

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,12 @@
+import pytest
+
 from .base_page import BasePage
 from .locators import LoginPageLocators
+import time
 
 
 class LoginPage(BasePage):
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -17,3 +21,14 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), 'Registration form is not presented'
+
+    def register_new_user(self, email, password):
+        input_email = self.browser.find_element(*LoginPageLocators.INPUT_EMAIL)
+        input_email.send_keys(email)
+        input_password = self.browser.find_element(*LoginPageLocators.INPUT_PASSWORD)
+        input_password.send_keys(password)
+        input_confirm_password = self.browser.find_element(*LoginPageLocators.INPUT_CONFIRM_PASSWORD)
+        input_confirm_password.send_keys(password)
+        button_registration = self.browser.find_element(*LoginPageLocators.BUTTON_REGISTRATION)
+        button_registration.click()
+        print(f'[+] User registered: {email}:{password}')
